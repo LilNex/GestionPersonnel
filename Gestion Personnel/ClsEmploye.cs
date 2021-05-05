31b499653519bd53aa2d8af25b10c129831825d3
MARIAM EL WAFI <ELWAFI.MARIAM@ofppt-edu.ma>
2021-05-05 21:59:36 +0000

    ajoute la classe employe

---
+++ Gestion Personnel/ClsEmploye.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Personnel
+{
+    class ClsEmploye
+    {
+        private string nom;
+        private String prenom;
+
+        public string Nom
+        {
+            get => nom; set
+            {
+                bool okey = false;
+                foreach (char c in value)
+                {
+                    if (char.IsLetter(c) == false)
+                    {
+                        okey = false;
+                    }
+                }
+                if (okey == false)
+                {
+                    throw new Exception("le Nom ne doit pas contenur des chiffres et des caracteres spécieux");
+                }
+                else
+                {
+                    nom = value;
+                }
+            }
+        }
+        public string Prenom
+        {
+            get => prenom; set
+            {
+                bool okey = false;
+                foreach (char c in value)
+                {
+                    if (char.IsLetter(c) == false)
+                    {
+                        okey = false;
+                    }
+                }
+                if (okey == false)
+                {
+                    throw new Exception("le Prenom ne doit pas contenur des chiffres et des caracteres spécieux");
+                }
+                else
+                {
+                    prenom = value;
+                }
+            }
+        }
+    }
+}
