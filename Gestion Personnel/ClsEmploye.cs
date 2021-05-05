a90704174b46f76795419048d61402f1a765f50d
Ismail Chaiq <CHAIQ.ISMAIL@ofppt-edu.ma>
2021-05-05 22:44:54 +0000

    update clsEmploye

--- Gestion Personnel/ClsEmploye.cs
+++ Gestion Personnel/ClsEmploye.cs
@@ -11,29 +11,22 @@ namespace Gestion_Personnel
         private string nom;
         private String prenom;
         private int num;
-
+        
         public string Nom
         {
-            get => nom; set
+            get => nom; 
+            set
             {
-                bool okey = false;
-                foreach (char c in value)
-                {
-                    if (char.IsLetter(c) == false)
-                    {
-                        okey = false;
-                    }
-                }
-                if (okey == false)
-                {
-                    throw new Exception("le Nom ne doit pas contenur des chiffres et des caracteres spécieux");
-                }
-                else
+                if (value.All(char.IsLetter))
                 {
                     nom = value;
                 }
+                else {
+                    throw new Exception("le Prenom ne doit pas contenur des chiffres et des caracteres spécieux");
+                }
             }
         }
+        
         public string Prenom
         {
             get => prenom; set
