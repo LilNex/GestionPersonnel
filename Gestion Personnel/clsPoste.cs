cac94ae4c1468a668d14ae0493fd1053e8d29051
MARIAM EL WAFI <ELWAFI.MARIAM@ofppt-edu.ma>
2021-05-06 13:09:51 +0000

    -Creé une classe(clsposte)avec les attribut en préver et la liste des employé -creéé les get et les set des attributs -crée le constrectur renesigné

---
+++ Gestion Personnel/clsPoste.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Personnel
+{
+    class clsPoste
+    {
+        private  string nom;
+        private int num;
+        public static List<ClsEmploye> listeDesEmployé;
+
+        public string Nom { get => nom; set => nom = value; }
+        public int Num { get => num; set => num = value; }
+
+        public clsPoste(string nom,int num)
+        {
+            this.nom = nom;
+            this.num = num;
+        }
+       
+       
+    }
+}
