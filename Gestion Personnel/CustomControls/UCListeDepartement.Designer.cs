c0eed2f705fdc0faa6e1f21cff1db25944fc3dd2
nextorchq <nextorchq@gmail.com>
2021-05-11 12:56:23 +0000

    UC Departemets

---
+++ Gestion Personnel/CustomControls/UCListeDepartement.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Gestion_Personnel.CustomControls
+{
+    partial class UCListeDepartement
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvListeEmployes = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtRechercheNom = new System.Windows.Forms.TextBox();
+            this.txtRechercheNum = new System.Windows.Forms.TextBox();
+            this.lblText_ParNom = new System.Windows.Forms.Label();
+            this.lblText_ParNumero = new System.Windows.Forms.Label();
+            this.lblText_Departements = new System.Windows.Forms.Label();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblText_Postes = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListeEmployes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvListeEmployes
+            // 
+            this.dgvListeEmployes.BackgroundColor = System.Drawing.Color.White;
+            this.dgvListeEmployes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvListeEmployes.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(174)))), ((int)(((byte)(238)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvListeEmployes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvListeEmployes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListeEmployes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvListeEmployes.EnableHeadersVisualStyles = false;
+            this.dgvListeEmployes.Location = new System.Drawing.Point(8, 70);
+            this.dgvListeEmployes.Name = "dgvListeEmployes";
+            this.dgvListeEmployes.RowHeadersVisible = false;
+            this.dgvListeEmployes.Size = new System.Drawing.Size(402, 414);
+            this.dgvListeEmployes.TabIndex = 30;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // txtRechercheNom
+            // 
+            this.txtRechercheNom.BackColor = System.Drawing.Color.DarkGray;
+            this.txtRechercheNom.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtRechercheNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtRechercheNom.Location = new System.Drawing.Point(145, 45);
+            this.txtRechercheNom.Name = "txtRechercheNom";
+            this.txtRechercheNom.Size = new System.Drawing.Size(124, 19);
+            this.txtRechercheNom.TabIndex = 28;
+            // 
+            // txtRechercheNum
+            // 
+            this.txtRechercheNum.BackColor = System.Drawing.Color.DarkGray;
+            this.txtRechercheNum.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtRechercheNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtRechercheNum.Location = new System.Drawing.Point(8, 45);
+            this.txtRechercheNum.Name = "txtRechercheNum";
+            this.txtRechercheNum.Size = new System.Drawing.Size(124, 19);
+            this.txtRechercheNum.TabIndex = 27;
+            // 
+            // lblText_ParNom
+            // 
+            this.lblText_ParNom.AutoSize = true;
+            this.lblText_ParNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText_ParNom.Location = new System.Drawing.Point(142, 25);
+            this.lblText_ParNom.Name = "lblText_ParNom";
+            this.lblText_ParNom.Size = new System.Drawing.Size(64, 16);
+            this.lblText_ParNom.TabIndex = 25;
+            this.lblText_ParNom.Text = "Par nom :";
+            // 
+            // lblText_ParNumero
+            // 
+            this.lblText_ParNumero.AutoSize = true;
+            this.lblText_ParNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText_ParNumero.Location = new System.Drawing.Point(5, 25);
+            this.lblText_ParNumero.Name = "lblText_ParNumero";
+            this.lblText_ParNumero.Size = new System.Drawing.Size(83, 16);
+            this.lblText_ParNumero.TabIndex = 24;
+            this.lblText_ParNumero.Text = "Par numéro :";
+            // 
+            // lblText_Departements
+            // 
+            this.lblText_Departements.AutoSize = true;
+            this.lblText_Departements.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText_Departements.Location = new System.Drawing.Point(3, 0);
+            this.lblText_Departements.Name = "lblText_Departements";
+            this.lblText_Departements.Size = new System.Drawing.Size(137, 24);
+            this.lblText_Departements.TabIndex = 23;
+            this.lblText_Departements.Text = "Departements :";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(174)))), ((int)(((byte)(238)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(550, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(402, 414);
+            this.dataGridView1.TabIndex = 31;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Column2";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Column3";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Column4";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // lblText_Postes
+            // 
+            this.lblText_Postes.AutoSize = true;
+            this.lblText_Postes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText_Postes.Location = new System.Drawing.Point(546, 0);
+            this.lblText_Postes.Name = "lblText_Postes";
+            this.lblText_Postes.Size = new System.Drawing.Size(76, 24);
+            this.lblText_Postes.TabIndex = 32;
+            this.lblText_Postes.Text = "Postes :";
+            // 
+            // UCListeDepartement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblText_Postes);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvListeEmployes);
+            this.Controls.Add(this.txtRechercheNom);
+            this.Controls.Add(this.txtRechercheNum);
+            this.Controls.Add(this.lblText_ParNom);
+            this.Controls.Add(this.lblText_ParNumero);
+            this.Controls.Add(this.lblText_Departements);
+            this.Name = "UCListeDepartement";
+            this.Size = new System.Drawing.Size(952, 529);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListeEmployes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListeEmployes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.TextBox txtRechercheNom;
+        private System.Windows.Forms.TextBox txtRechercheNum;
+        private System.Windows.Forms.Label lblText_ParNom;
+        private System.Windows.Forms.Label lblText_ParNumero;
+        private System.Windows.Forms.Label lblText_Departements;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.Label lblText_Postes;
+    }
+}
---
+++ Gestion Personnel/CustomControls/_UCDepartements.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Gestion_Personnel.CustomControls
+{
+    partial class _UCDepartements
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAjouterEmploye = new System.Windows.Forms.Button();
+            this.btnListeEmployes = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAjouterEmploye
+            // 
+            this.btnAjouterEmploye.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(174)))), ((int)(((byte)(238)))));
+            this.btnAjouterEmploye.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAjouterEmploye.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjouterEmploye.ForeColor = System.Drawing.Color.White;
+            this.btnAjouterEmploye.Image = global::Gestion_Personnel.Properties.Resources.icons8_add_user_male_32px;
+            this.btnAjouterEmploye.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAjouterEmploye.Location = new System.Drawing.Point(450, 0);
+            this.btnAjouterEmploye.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAjouterEmploye.Name = "btnAjouterEmploye";
+            this.btnAjouterEmploye.Size = new System.Drawing.Size(225, 42);
+            this.btnAjouterEmploye.TabIndex = 7;
+            this.btnAjouterEmploye.Text = "Ajouter un départements";
+            this.btnAjouterEmploye.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAjouterEmploye.UseVisualStyleBackColor = false;
+            // 
+            // btnListeEmployes
+            // 
+            this.btnListeEmployes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(174)))), ((int)(((byte)(238)))));
+            this.btnListeEmployes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListeEmployes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListeEmployes.ForeColor = System.Drawing.Color.White;
+            this.btnListeEmployes.Image = global::Gestion_Personnel.Properties.Resources.icons8_user_groups_32px;
+            this.btnListeEmployes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnListeEmployes.Location = new System.Drawing.Point(0, 0);
+            this.btnListeEmployes.Margin = new System.Windows.Forms.Padding(0);
+            this.btnListeEmployes.Name = "btnListeEmployes";
+            this.btnListeEmployes.Size = new System.Drawing.Size(225, 42);
+            this.btnListeEmployes.TabIndex = 6;
+            this.btnListeEmployes.Text = "Liste des départements";
+            this.btnListeEmployes.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnListeEmployes.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(174)))), ((int)(((byte)(238)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = global::Gestion_Personnel.Properties.Resources.icons8_user_groups_32px;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(225, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 42);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Liste des postes";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(174)))), ((int)(((byte)(238)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = global::Gestion_Personnel.Properties.Resources.icons8_add_user_male_32px;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(675, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 42);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Ajouter un poste";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // _UCDepartements
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAjouterEmploye);
+            this.Controls.Add(this.btnListeEmployes);
+            this.Controls.Add(this.button1);
+            this.Name = "_UCDepartements";
+            this.Size = new System.Drawing.Size(952, 569);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAjouterEmploye;
+        private System.Windows.Forms.Button btnListeEmployes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
